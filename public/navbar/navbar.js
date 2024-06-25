c33a72236e304a5e0e560e955967fc156e83d34f
jonnyjackson26 <jrsjackson26@gmail.com>
2024-06-25 14:50:35 -0700

    removed our tours dropdown on navbar

--- public/navbar/navbar.js
+++ public/navbar/navbar.js
@@ -7,3 +7,16 @@ function loadNavbar() {
 }
 
 document.addEventListener('DOMContentLoaded', loadNavbar);
+
+
+
+/*
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navList = document.querySelector('.nav-list');
+
+    mobileMenu.addEventListener('click', () => {
+        mobileMenu.classList.toggle('active'); // Toggle active class on the menu-toggle itself
+        navList.classList.toggle('active'); // Toggle active class on the nav-list
+    });
+});*/
